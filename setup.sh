f65b18cf3eb87c79d6379a31a350a965cb8ce689
Brian Spiering <bspiering@gmail.com>
2018-08-18 06:38:36 -0700

    Fix spelling mistakes 😊

--- setup.sh
+++ setup.sh
@@ -9,14 +9,6 @@ conda env create --name keras --force
 # Start environment
 source activate keras
 
-# Setup spell checking and other notebook enhancements
-git clone https://github.com/Calysto/notebook-extensions.git
-cd notebook-extensions
-jupyter nbextension install calysto --user
-jupyter nbextension enable calysto/spell-check/main
-jupyter nbextension enable calysto/cell-tools/main
-jupyter nbextension enable calysto/annotate/main
-
 # Setup RISE (https://github.com/damianavila/RISE) slideshows 
 jupyter nbextension install rise --py --sys-prefix
 jupyter nbextension enable rise --py --sys-prefix
